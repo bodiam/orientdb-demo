411ada410e84d4225aa8f1cc176145b8add31cfa
Erik Pragt <erik.pragt@jworks.nl>
2015-12-27 01:40:29 +0100

    Added empty CRUD repo

--- src/main/java/com/orientdb/demo/repository/object/ObjectAuthorRepository.java
+++ src/main/java/com/orientdb/demo/repository/object/ObjectAuthorRepository.java
@@ -1,10 +1,52 @@
 package com.orientdb.demo.repository.object;
 
+import com.orientdb.demo.domain.Author;
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import java.util.List;
+
 /**
  * @author Erik Pragt
  */
 public class ObjectAuthorRepository {
 
+    public long count() {
+        return 0;
+    }
 
+    public void delete(String rid) {
 
+    }
+
+    public void delete(Iterable<? extends Author> entities) {
+
+    }
+
+    public void delete(Author author) {
+
+    }
+
+    public boolean 	exists(String id) {
+        return false;
+    }
+
+    public Iterable<Author> findAll() {
+        return null;
+    }
+
+    public Iterable<Author> findAll(Iterable<ID> ids) {
+        return null;
+    }
+
+    public Author findOne(String id) {
+        return null;
+    }
+
+    public void save(Iterable<Author> entities) {
+
+    }
+
+    public void save(Author author) {
+
+    }
 }
